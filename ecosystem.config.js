d5a439a1746ebe7ffded2278dc4ff7e4b791ac2a
roycekings <zeparas34@gmail.com>
2025-06-20 18:58:26 +0100

    modified:   .env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     max_memory_restart: "1G",
     env: {
       PORT: 5000,
-      MONGO_URI: 'mongodb://root:root@192.168.100.1:27017/loginDb?authSource=admin&directConnection=true',
+      MONGO_URI: 'mongodb://root:root@localhost:27017/loginDb?authSource=admin&directConnection=true',
       JWT_SECRET:'your_jwt_secret_key',
       JWT_EXPIRE: '30d'
 
